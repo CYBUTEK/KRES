0cef674d5c77dda750bee396a381cdd7c855d4bc
stupid_chris <christophe_savard@hotmail.ca>
2014-01-05 11:47:28 -0500

    update Animate.cs

---
+++ KRES/Animate.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace KRES
+{
+    public static class Animate
+    {
+        static void InitiateAnimation(Part part, string animationName)
+        {
+            foreach (Animation anim in part.FindModelAnimators(animationName))
+            {
+                AnimationState state = anim.animation[animationName];
+                state.normalizedSpeed = 0;
+                state.normalizedTime = 0;
+                state.layer = 1;
+                state.enabled = false;
+            }
+        }
+
+        #region PlayAnimation()
+        public static void PlayAnimation(Part part, string animationName, float animationSpeed)
+        {
+            foreach (Animation anim in part.FindModelAnimators(animationName))
+            {
+                AnimationState state = anim.animation[animationName];
+                state.normalizedSpeed = animationSpeed;
+                state.normalizedTime = 0;
+                state.wrapMode = WrapMode.Clamp;
+                state.enabled = true;
+                anim.Play(animationName);
+            }
+        }
+
+        public static void PlayAnimation(Part part, string animationName, float animationSpeed, float animationTime)
+        {
+            foreach (Animation anim in part.FindModelAnimators(animationName))
+            {
+                AnimationState state = anim.animation[animationName];
+                state.normalizedSpeed = animationSpeed;
+                state.normalizedTime = animationTime;
+                state.wrapMode = WrapMode.Clamp;
+                state.enabled = true;
+                anim.Play(animationName);
+            }
+        }
+
+        public static void PlayAnimation(Part part, string animationName, float animationSpeed, WrapMode mode)
+        {
+            foreach (Animation anim in part.FindModelAnimators(animationName))
+            {
+                AnimationState state = anim.animation[animationName];
+                state.normalizedSpeed = animationSpeed;
+                state.normalizedTime = 0;
+                state.wrapMode = mode;
+                state.enabled = true;
+                anim.Play(animationName);
+            }
+        }
+
+        public static void PlayAnimation(Part part, string animationName, float animationSpeed, float animationTime, WrapMode mode)
+        {
+            foreach (Animation anim in part.FindModelAnimators(animationName))
+            {
+                AnimationState state = anim.animation[animationName];
+                state.normalizedSpeed = animationSpeed;
+                state.normalizedTime = animationTime;
+                state.wrapMode = mode;
+                state.enabled = true;
+                anim.Play(animationName);
+            }
+        }
+        #endregion
+
+        public static bool CheckAnimationPlaying(Part part, string animationName)
+        {
+            bool isPlaying = false;
+            foreach (Animation anim in part.FindModelAnimators(animationName)) { isPlaying = anim.isPlaying; }
+            return isPlaying;
+        }
+
+        public static float GetAnimationTime(Part part, string animationName)
+        {
+            float time = 0;
+            foreach (Animation anim in part.FindModelAnimators(animationName)) { time = anim.animation[animationName].normalizedTime; }
+            return time;
+        }
+    }
+}
